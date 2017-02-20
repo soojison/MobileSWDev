9ad6f12504b7e4df175928ff3cb7058d737f6db9
Sooji Son <soojison96@gmail.com>
2017-02-20 12:41:37 +0100

    red circles

--- TicTacToe2/app/src/main/java/hu/ait/tictactoe/view/TicTacToeView.java
+++ TicTacToe2/app/src/main/java/hu/ait/tictactoe/view/TicTacToeView.java
@@ -18,6 +18,7 @@ public class TicTacToeView extends View {
     // factor out so it doesn't get created every time onDraw is called
     private Paint paintBg;
     private Paint paintLine;
+    private Paint paintRedCircle;
 
     private List<PointF> coords = new ArrayList<PointF>();
 
@@ -32,6 +33,10 @@ public class TicTacToeView extends View {
         paintLine.setColor(Color.WHITE);
         paintLine.setStyle(Paint.Style.STROKE);
         paintLine.setStrokeWidth(5);
+
+        paintRedCircle = new Paint();
+        paintRedCircle.setColor(Color.RED);
+        paintRedCircle.setStyle(Paint.Style.FILL);
     }
 
     @Override
@@ -44,8 +49,9 @@ public class TicTacToeView extends View {
 
         // shortcut: coords.for
         for (PointF coord : coords) {
-            canvas.drawCircle(coord.x, coord.y, 50, paintLine);
+            //canvas.drawCircle(coord.x, coord.y, 50, paintLine);
             // Pekler: "HAHA! I have circles"
+            canvas.drawCircle(coord.x, coord.y, 50, paintRedCircle);
         }
         
     }
