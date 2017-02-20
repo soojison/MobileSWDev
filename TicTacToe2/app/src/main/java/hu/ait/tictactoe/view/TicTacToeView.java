374beecb8acf7f321f9418daf08a08d722fa5de5
Sooji Son <soojison96@gmail.com>
2017-02-20 12:32:33 +0100

    clicky circles

--- TicTacToe2/app/src/main/java/hu/ait/tictactoe/view/TicTacToeView.java
+++ TicTacToe2/app/src/main/java/hu/ait/tictactoe/view/TicTacToeView.java
@@ -4,24 +4,65 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicTacToeView extends View {
 
+    // factor out so it doesn't get created every time onDraw is called
+    private Paint paintBg;
+    private Paint paintLine;
+
+    private List<PointF> coords = new ArrayList<PointF>();
+
     public TicTacToeView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        paintBg = new Paint();
+        paintBg.setColor(Color.BLACK);
+        paintBg.setStyle(Paint.Style.FILL); // fills the area
+
+        paintLine = new Paint();
+        paintLine.setColor(Color.WHITE);
+        paintLine.setStyle(Paint.Style.STROKE);
+        paintLine.setStrokeWidth(5);
     }
 
     @Override
     protected void onDraw(Canvas canvas) { // the entire drawable area
         super.onDraw(canvas);
 
-        // draw a rectangle
-        Paint paintBg = new Paint();
-        paintBg.setColor(Color.BLACK);
-        paintBg.setStyle(Paint.Style.FILL); // fills the area
-
         canvas.drawRect(0, 0, getWidth(), getHeight(), paintBg);
+
+        canvas.drawLine(0, 0, getWidth(), getHeight(), paintLine);
+
+        // shortcut: coords.for
+        for (PointF coord : coords) {
+            canvas.drawCircle(coord.x, coord.y, 50, paintLine);
+            // Pekler: "HAHA! I have circles"
+        }
+        
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            coords.add(new PointF(event.getX(), event.getY()));
+            
+            // invalidate redraws everything on the view
+            invalidate();
+            
+            
+        }
+
+        return true;
+
     }
 }
