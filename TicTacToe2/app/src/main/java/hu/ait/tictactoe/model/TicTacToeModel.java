8d088da3da776f5b1561a75aa7d2f9f23c32bf44
Sooji Son <soojison96@gmail.com>
2017-02-23 16:40:09 +0100

    first draft of the model

---
+++ TicTacToe2/app/src/main/java/hu/ait/tictactoe/model/TicTacToeModel.java
@@ -0,0 +1,70 @@
+package hu.ait.tictactoe.model;
+
+// A singleton model = only one instance of it can exist
+
+public class TicTacToeModel {
+
+    // we should have one instance of this
+    private static TicTacToeModel instance = null;
+
+    // private constructor: you cannot create a new instance of this in the main method
+    private TicTacToeModel() {
+
+    }
+
+    // singleton design pattern: if there is no object, then create it
+    // otherwise do not do anything because you want only one instance of the model
+    public static TicTacToeModel getInstance() {
+        if (instance == null) {
+            instance = new TicTacToeModel();
+        }
+        return instance;
+    }
+
+    // creating constants in java (use the final keyword so it's uneditable)
+    public static final short EMPTY =  0;
+    public static final short CIRCLE = 1;
+    public static final short CROSS = 2;
+
+    private short[][] model = {
+            {EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY}
+    };
+
+    // circle will be the first player
+    private short nextPlayer = CIRCLE;
+
+    public void resetModel() {
+        for (int i = 0; i < 3; i++) {           //fori + enter = shortcut for creating for loops
+            for (int j = 0; j < 3; j++) {
+                model[i][j] = EMPTY;
+            }
+        }
+        nextPlayer = CIRCLE;
+
+        // you could also do this, but this causes more work for garbage collector:
+        //      instance = null;
+    }
+
+    public short checkWinner() {
+        return 0;
+    }
+
+    public void changeNextPlayer() {
+        nextPlayer = (nextPlayer == CIRCLE) ? CROSS : CIRCLE;
+    }
+
+    public void setField(int x, int y, short player) {
+        model[x][y] = player;
+    }
+
+    public short getField(int x, int y) {
+        return model[x][y];
+    }
+
+    // cmd + n: auto getter setter generator
+    public short getNextPlayer() {
+        return nextPlayer;
+    }
+}
