c050391eede2df28d58129a3d3ea7f84d24ee829
Sooji Son <soojison96@gmail.com>
2017-03-02 17:19:11 +0100

    start game activity

--- HighLowGame/app/src/main/java/io/github/soojison/highlowgame/MainActivity.java
+++ HighLowGame/app/src/main/java/io/github/soojison/highlowgame/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.soojison.highlowgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        Button btnHelp = (Button) findViewById(R.id.btnHelp);
+        Button btnAbout = (Button) findViewById(R.id.btnHelp);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start the game activity
+                Intent intentStartGame = new Intent();
+                intentStartGame.setClass(MainActivity.this, GameActivity.class);
+                startActivity(intentStartGame);
+            }
+        });
     }
 }
