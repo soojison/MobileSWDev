ead38c7cc8b113206d6a175542e9f2174071d409
Sooji Son <soojison96@gmail.com>
2017-03-19 12:16:04 +0100

    end of app

--- TodoInflaterDemo/app/src/main/java/hu/ait/todoinflaterdemo/MainActivity.java
+++ TodoInflaterDemo/app/src/main/java/hu/ait/todoinflaterdemo/MainActivity.java
@@ -30,12 +30,26 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnSave)
     public void savePressed(Button btn) {
-        View viewTodo = // representing one line of the todo item
+        final View viewTodo = // representing one line of the to-do item
                 getLayoutInflater().inflate(R.layout.layout_todo, null);
 
         TextView tvTodo = (TextView) viewTodo.findViewById(R.id.tvTodo);
         tvTodo.setText(etTodo.getText().toString());
 
+        // don't forget to do viewTodo.findViewById because if you just do
+        // findViewById by itself tries to find it in the original layout
+        // and the button does not exist
+        Button btnDel = (Button) viewTodo.findViewById(R.id.btnDelete);
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // must remove the list from the layoutContent
+                layoutContent.removeView(viewTodo);
+            }
+        });
+
         layoutContent.addView(viewTodo);
+        // if you want to add the new item to the top, you can use
+        // layoutContent.addView(viewTodo, 0);
     }
 }
