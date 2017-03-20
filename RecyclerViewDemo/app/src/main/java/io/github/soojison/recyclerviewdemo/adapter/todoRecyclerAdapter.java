493ad14f8057d20b394900f03bc6cfe7aa7a292f
Sooji Son <soojison96@gmail.com>
2017-03-20 11:20:43 +0100

    RecyclerViewDemo: start adapter and layout row

---
+++ RecyclerViewDemo/app/src/main/java/io/github/soojison/recyclerviewdemo/adapter/todoRecyclerAdapter.java
@@ -0,0 +1,15 @@
+package io.github.soojison.recyclerviewdemo.adapter;
+
+import java.util.List;
+
+import io.github.soojison.recyclerviewdemo.data.Todo;
+
+/**
+ * The most important class in our code.
+ * Please get familiar with this stuff!
+ */
+
+public class todoRecyclerAdapter {
+
+    private List<Todo> todoList;
+}
---
+++ RecyclerViewDemo/app/src/main/java/io/github/soojison/recyclerviewdemo/data/Todo.java
@@ -0,0 +1,33 @@
+package io.github.soojison.recyclerviewdemo.data;
+
+/**
+ * Contains info about the to-do item
+ */
+
+public class Todo {
+
+    private String todoText;
+    private boolean done;
+
+    public Todo(String todoText, boolean done) {
+        this.todoText = todoText;
+        this.done = done;
+    }
+
+    public String getTodoText() {
+        return todoText;
+    }
+
+    public void setTodoText(String todoText) {
+        this.todoText = todoText;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+}
