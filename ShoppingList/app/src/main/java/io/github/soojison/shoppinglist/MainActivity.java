c0c585b39e550b0dc384fec33aaa982cff04400f
Sooji Son <soojison96@gmail.com>
2017-04-03 21:42:47 +0200

    ShoppingList: add addActivity

--- ShoppingList/app/src/main/java/io/github/soojison/shoppinglist/MainActivity.java
+++ ShoppingList/app/src/main/java/io/github/soojison/shoppinglist/MainActivity.java
@@ -1,8 +1,12 @@
 package io.github.soojison.shoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menuAdd:
+                startActivity(new Intent().setClass(MainActivity.this, AddActivity.class));
+                break;
+            case R.id.menuDeleteAll:
+                Toast.makeText(this, "Deleteall", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
