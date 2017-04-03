c3bf41de99ad2d57f4182c4d2e7e303f251e3a29
Sooji Son <soojison96@gmail.com>
2017-04-03 21:58:19 +0200

    ShoppingList: toolbar tweaks

--- ShoppingList/app/src/main/java/io/github/soojison/shoppinglist/AddActivity.java
+++ ShoppingList/app/src/main/java/io/github/soojison/shoppinglist/AddActivity.java
@@ -3,6 +3,7 @@ package io.github.soojison.shoppinglist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -13,5 +14,18 @@ public class AddActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Add New Item");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
