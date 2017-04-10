a997931fd41c8cfcf93474b2183d055d0b38b56d
Sooji Son <soojison96@gmail.com>
2017-04-10 12:24:17 +0200

    AnimationDemo: Working animation added

--- AnimationDemo/app/src/main/java/io/github/soojison/animationdemo/MainActivity.java
+++ AnimationDemo/app/src/main/java/io/github/soojison/animationdemo/MainActivity.java
@@ -2,6 +2,10 @@ package io.github.soojison.animationdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnPlay = (Button) findViewById(R.id.btnPlay);
+        final Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.play_anim);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnPlay.startAnimation(animation);
+            }
+        });
     }
 }
