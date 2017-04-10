21f337aeb36238718591d7b49b727e18c7cf086d
Sooji Son <soojison96@gmail.com>
2017-04-10 12:32:20 +0200

    AnimationDemo: misc tricks with animations

--- AnimationDemo/app/src/main/java/io/github/soojison/animationdemo/MainActivity.java
+++ AnimationDemo/app/src/main/java/io/github/soojison/animationdemo/MainActivity.java
@@ -6,8 +6,18 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // pekler: "how do we play one animation and then the other? It's a requirement from the customer"
+    // pekler: "We don't do it. Go to hell. I leave this job."
+
+    // pekler: design team has done a good job, it's always the developers who have to implement. that's life
+
+    // how to play the animation and then start another activity?
+    // waht? how can we do dat? we can give an event to the animation
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 btnPlay.startAnimation(animation);
+            }
+        });
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // Here, you can start a new activity or do whatever!
+                // WAY simpler than using Timers and thinking about UI Thread
+                Toast.makeText(MainActivity.this, "Animation ended", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
             }
         });
     }
